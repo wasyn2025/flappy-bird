9cb91320308c82738c12b0aadf7f8ca1659a91f6
wasyn2025 <wasynssulaiman@gmail.com>
2026-05-31 11:56:31 +0700

    Added collision detection

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
-canvas.width = 800;
+canvas.width = 900;
 canvas.height = 550;
-canvas.style.border = "2px solid black";
-canvas.style.margin = "30px auto";
-canvas.style.display = "block";
 
-let gameOverAudio = null;
-let birdPositionY = 0;
 let isStart = false;
 const pipes = [];
 const minPipeHeight = 20;
 const pipeGap = 155;
 const pipeWidth = 70;
 const maxPipeHeight = canvas.height - pipeGap - minPipeHeight;
-const pipeSpawnRange = [(canvas.width / 2) - 4, canvas.width / 2];
+const pipeDistance = (canvas.width / 2) <= 300 ? (canvas.width / 2) - 50 : (canvas.width / 2) + 100;
+const pipeMoveSpeed = 2.5;
+const pipeSpawnRange = [pipeDistance - (pipeMoveSpeed + 2), pipeDistance];
 
 const bird = {
     positionX: 100,
     positionY: 50,
     flapAudio: null,
     hitGroundAudio: null,
+    gameOverAudio: null,
     velocityY: 0,
     gravity: 0.6,
 }
@@ -51,7 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function startGame() {
     if (bird.positionY >= (canvas.height - 50)) {
         bird.hitGroundAudio.cloneNode().play();
-        setTimeout(() => gameOverAudio.cloneNode().play(), 450);
+        setTimeout(() => bird.gameOverAudio.cloneNode().play(), 450);
 
         isStart = false;
         context.fillStyle = "red";
@@ -70,25 +68,48 @@ function startGame() {
     for (let i = 0; i < pipes.length; i++) {
         if (pipes[i].x > pipeSpawnRange[0] && pipes[i].x < pipeSpawnRange[1]) {
             generatePipes();
-            console.log(pipes);
         }
 
         if (pipes[i].x + pipes[i].width < 0) {
             pipes.splice(i, 1);
         }
 
-        pipes[i].x -= 2;
+        if (isColliding(pipes[i])) {
+            isStart = false;
+            bird.gameOverAudio.cloneNode().play();
+            context.fillStyle = "red";
+            context.fillRect(bird.positionX, bird.positionY, 50, 50)
+
+            return;
+        }
+
+        pipes[i].x -= pipeMoveSpeed;
 
         context.fillRect(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height); // pipe atas
         context.fillRect(
             pipes[i].x,
             (pipes[i].height + pipes[i].gap),
-            pipes[0].width,
+            pipes[i].width,
             canvas.height - (pipes[i].height + pipes[i].gap)
         ); //  pipe bawah
     }
 
     requestAnimationFrame(startGame);
+}
+
+function isColliding(pipe) {
+    const birdRect = {
+        x: bird.positionX,
+        y: bird.positionY,
+        width: 50,
+        height: 50
+    };
+
+    return (
+        birdRect.x < pipe.x + pipe.width &&
+        birdRect.x + birdRect.width > pipe.x &&
+        (birdRect.y < pipe.y + pipe.height || birdRect.y + birdRect.height > pipe.y + pipe.height + pipe.gap)
+    );
 }
 
 function generatePipes() {
@@ -112,7 +133,7 @@ function clearPipes() {
 function loadAudio() {
     bird.hitGroundAudio = new Audio("./sounds/hit-ground.wav");
     bird.flapAudio = new Audio("./sounds/bird-flap.wav");
-    gameOverAudio = new Audio("./sounds/gameover.wav");
+    bird.gameOverAudio = new Audio("./sounds/gameover.wav");
 }
 
 function prepareGame() {
