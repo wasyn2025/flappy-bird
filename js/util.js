8e230da3f5a499873b906a1560e3ab4876f7d64b
wasyn2025 <wasynssulaiman@gmail.com>
2026-07-28 01:24:20 +0700

    DELETE FEATURE: Delete bird displaying at start menu

--- js/util.js
+++ js/util.js
@@ -113,17 +113,17 @@ export function runBackground() {
     drawBackground();
     drawGround();
 
-    context.drawImage(
-        assets.bird,
-        0,
-        0,
-        16,
-        16,
-        state.bird.positionX,
-        100,
-        state.bird.width,
-        state.bird.height
-    );
+    // context.drawImage(
+    //     assets.bird,
+    //     0,
+    //     0,
+    //     16,
+    //     16,
+    //     state.bird.positionX,
+    //     100,
+    //     state.bird.width,
+    //     state.bird.height
+    // );
 
     state.backgroundAutoRunId = requestAnimationFrame(runBackground);
 }
